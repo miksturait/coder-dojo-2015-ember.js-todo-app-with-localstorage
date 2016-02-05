76b6beddaa6101aa41e5d09c86725af7cab2d4f4
Michal Czyz <michalczyz@gmail.com>
2016-02-05 15:26:26 +0100

    todo widget fix

--- app/components/to-do-widget.js
+++ app/components/to-do-widget.js
@@ -7,6 +7,7 @@ export default Ember.Component.extend({
     addTodo(text){
       if (this.get('session.isAuthenticated')) {
         this.get('store').createRecord('todo', {text: text}).save();
+        return(true);
       } else {
         alert('please log in :-)');
         return(false);
