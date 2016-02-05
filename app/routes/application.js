367d8c07b1995d276cd6f2b508bd09d764e317e6
Michal Czyz <michalczyz@gmail.com>
2016-02-05 15:28:57 +0100

    after successful authentication let's create (or update if already present) user model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.get("session").fetch().catch(function() {});
+  store: Ember.inject.service(),
+
+  beforeModel() {
+    return this.get("session").fetch().catch(function () {
+    });
+  },
+  findOrCreateUser(data) {
+    this.store.find('user', data.uid).then(
+      function (user) {
+        user.set('name', data.currentUser.displayName);
+        user.set('imageUrl', data.currentUser.profileImageURL);
+        user.save();
+      },
+      () => this.store.createRecord('user',
+        {id: data.uid, name: data.currentUser.displayName, imageUrl: data.currentUser.profileImageURL}).save()
+    );
   },
 
   actions: {
-    signIn: function(provider) {
-      this.get("session").open("firebase", { provider: provider}).then(function(data) {
-        console.log(data.currentUser);
-      });
+    signIn(provider) {
+      this.get("session").open("firebase", {provider: provider}).then((data) => this.findOrCreateUser(data));
     },
-    signOut: function() {
+    signOut() {
       this.get("session").close();
     }
   }
