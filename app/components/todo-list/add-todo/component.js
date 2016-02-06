44ae8228ac9df6945ce9ee6139484f31b9eb2f95
Michal Czyz <michalczyz@gmail.com>
2016-02-06 08:45:19 +0100

    generate todo model and components

--- app/components/todo-list/add-todo/component.js
+++ app/components/todo-list/add-todo/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    submit() {
+      let description = this.get('description');
+      if (this.attrs.addTodo(description)) {
+        this.set('description', '');
+      }
+    }
+  }
 });
--- app/components/todo-list/component.js
+++ app/components/todo-list/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    addTodo(description) {
+      let store = this.get('store');
+
+      store.createRecord('todo', {description: description});
+      return true;
+    }
+  }
 });
